0e6faa85e11ecd41f780b2099fd4178cec774175
Krishna <kvrmoorthi.93@gmail.com>
2024-09-24 09:42:47 +0530

    First

--- makemore.py
+++ makemore.py
@@ -1,16 +1,9 @@
-"""
-you give this script some words (one per line) and it will generate more things like it.
-uses super state of the art Transformer AI tech
-this code is intended to be super hackable. tune it to your needs.
-"""
-
+import argparse
+import math
 import os
 import sys
 import time
-import math
-import argparse
 from dataclasses import dataclass
-from typing import List
 
 import torch
 import torch.nn as nn
@@ -18,6 +11,7 @@ from torch.nn import functional as F
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+
 
 # -----------------------------------------------------------------------------
 
